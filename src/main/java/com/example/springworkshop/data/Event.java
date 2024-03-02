1f52f7c6eecf1b53527806c8ee864c5ca9df4707
HamzaGbada <hamza.gbada@gmail.com>
2024-03-02 15:03:58 +0100

    init

--- src/main/java/com/example/springworkshop/data/Event.java
+++ src/main/java/com/example/springworkshop/data/Event.java
@@ -1,4 +1,30 @@
 package com.example.springworkshop.data;
 
-public class Event {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Event implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+    private Date date;
+    private String place;
+    private String subject;
+
 }
--- src/main/java/com/example/springworkshop/data/Member.java
+++ src/main/java/com/example/springworkshop/data/Member.java
@@ -1,8 +1,28 @@
 package com.example.springworkshop.data;
 
-public class Member {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Member implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String name;
     private String email;
     private int CIN;
+
+
 }
